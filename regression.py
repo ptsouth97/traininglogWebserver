4301ef08d8b88dcd278f9ed9ed79f1bb154ee27d
ptsouth97 <ptsouth97@gmail.com>
2023-04-30 13:46:18 -0400

    added new regression analysis

--- regression.py
+++ regression.py
@@ -17,11 +17,27 @@ def main():
 	
 	df = load.to_df(filename)
 
+	variables = ['Elevation (ft)', 'Temperature (F)', 'Humidity (%)',
+				 'Feels like', 'Distance (miles)', 'Time', 
+				 'Average Pace (min/mile)', 'GARMIN Average HR (bpm)',
+				 'Average Cadence (spm)', 'Average Power (W)',
+				 'Average Vertical Oscillation (cm)', 
+				 'Average Ground Contact Time (ms)', 'Max HR',
+				 'Average Stride Length (m)']
+
+	for variable1 in variables:
+		for variable2 in variables:
+			if variable1 == variable2:
+				continue
+			print("Calculating regression for " + variable1 + " and " + variable2)
+			two_variable_correlation(df, variable1, variable2)
+
+	'''
 	variable1 = 'Average Cadence (spm)'
 	variable2 = 'GARMIN Average HR (bpm)'
 
 	two_variable_correlation(df, variable1, variable2)
-
+	'''
 	return
 
 
@@ -29,24 +45,28 @@ def two_variable_correlation(df, variable1, variable2):
 	''' Plots one variable versus another to test correlation'''
 
 	# Exclude outliers in the 1% range
+	'''
 	q1 = df[variable1].quantile(0.1)
 	df = df[df[variable1] > q1]
 
 	q2 = df[variable2].quantile(0.9)
 	df = df[df[variable2] < q2]
+	'''
 
 	df.plot(x=variable1, y=variable2, kind='scatter')
 
 	df = df.dropna(subset=[variable1, variable2])
 
-	column = df[variable1]
+	column = pd.to_numeric(df[variable1])
 	upper_end = column.max()
 	lower_end = column.min()
 
 	reg = LinearRegression()
 	prediction_space = np.linspace(lower_end, upper_end).reshape(-1,1)
-	X = df[variable1].values.reshape(-1,1)
-	y = df[variable2].values.reshape(-1,1)
+	X = pd.to_numeric(df[variable1]).values.reshape(-1,1)
+	y = pd.to_numeric(df[variable2]).values.reshape(-1,1)
+	
+	
 	reg.fit(X,y)
 	y_pred = reg.predict(prediction_space)
 
